359d745c49dabfdf37e894319944acee46a1d5fb
Clara-Musyoka <musyokamusenya@gmail.com>
2022-03-10 13:05:16 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ * printf:This is what is used to display
+ * Description:print without puts or printf
+ *Return:Then return 0
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
