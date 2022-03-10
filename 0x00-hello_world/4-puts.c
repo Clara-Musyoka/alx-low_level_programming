df7f9afd1fd2a198be3d346c7590b33eb141a23f
Clara-Musyoka <musyokamusenya@gmail.com>
2022-03-10 11:39:55 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-include <stdio.h>
+#include <stdio.h>
+/*Printing a string of characters*/
 
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);	
 }
