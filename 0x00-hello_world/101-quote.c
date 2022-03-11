79cb6e2a5716bf9245885a70ab7d2d058f96495f
Clara-Musyoka <musyokamusenya@gmail.com>
2022-03-11 10:47:23 -0800

    correcte

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main -Entry point
  * printf:This is what is used to display
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful \" 
-			- Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful
+		       \" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
