4fe9484053b61debdda0d6ff2d1ac6ebbd7aa11f
Clara-Musyoka <musyokamusenya@gmail.com>
2022-04-11 10:09:33 -0700

    pre

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints the name of file
- * Return: Always 0 (Success)
+ * main - entry point
+ * Return: always 0
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
